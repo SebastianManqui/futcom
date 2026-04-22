1e07781507c940798a920accdfb5c9e9551e5c5e
SebastianManqui <se.manqui@duocuc.cl>
2026-04-22 13:21:36 -0400

    cambios

---
+++ futcom/src/main/java/com/duoc/futcom/FutcomApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.futcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutcomApplication.class, args);
+	}
+
+}
